4644ed4f8e799a28a72dfba3e8d3bd00f5cfe992
Navi <mailvoornavi@gmail.com>
2021-12-05 21:01:29 +0100

    added basic file functions

--- srcs/utils/file.c
+++ srcs/utils/file.c
@@ -1,26 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   pipex.h                                            :+:    :+:            */
+/*   file.c                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2021/11/23 00:27:03 by nismail       #+#    #+#                 */
-/*   Updated: 2021/12/05 20:54:55 by navi          ########   odam.nl         */
+/*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
+/*   Updated: 2021/12/05 20:57:25 by navi          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../../includes/pipex.h"
 
-// REMOVE THIS LINE!!
-# include <stdio.h>
+/*
+ * The ft_open() function ...
+ */
+int	ft_open(char *file)
+{
+	int	fd;
 
-# include <unistd.h>
-# include <sys/types.h>
-# include <fcntl.h>
+	fd = open(file, O_RDWR);
+	if (fd < 0)
+		return (-1);
+	return (1);
+}
 
-int		ft_open(char *file);
-void	ft_close(int fd);
-
-#endif
+/*
+ * The ft_close() function ...
+ */
+void	ft_close(int fd)
+{
+	close(fd);
+}
