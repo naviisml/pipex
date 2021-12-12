c90c98d4860bbb94a0c16e532bebe6be4f6a8803
Navi Ismail <mailvoornavi@gmail.com>
2021-12-12 17:43:18 +0100

    code cleanup

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:27:03 by nismail       #+#    #+#                 */
-/*   Updated: 2021/12/06 17:19:05 by navi          ########   odam.nl         */
+/*   Updated: 2021/12/12 17:40:34 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@
 // REMOVE THIS LINE!!
 # include <stdio.h>
 
+// System Libraries...
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <errno.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <sys/types.h>
 # include <fcntl.h>
 
+// Pipex Struct...
 typedef struct s_pipex
 {
 	int		fd_input;
@@ -29,6 +33,7 @@ typedef struct s_pipex
 	int		cmdc;
 }	t_pipex;
 
+// Function Prototypes...
 int		ft_file_open(char *file);
 int		ft_file_create(char *file);
 void	ft_file_close(int fd);
