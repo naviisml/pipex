6faf406e8445415d4fc8e68256b7ecbf356f9aba
Navi Ismail <mailvoornavi@gmail.com>
2022-02-06 17:41:44 +0100

    added `process_exec` function
    - Executes a process
    - Save the output to a output file (temporary)

--- srcs/modules/process.c
+++ srcs/modules/process.c
@@ -12,6 +12,28 @@
 
 #include <pipex.h>
 
+/**
+ * The process_exec() function ...
+ * 
+ * Step 1. Execute command in execve
+ * Step 2. Save the output to the struct
+ * Step 3. Use the output on the next command
+ * ...
+ * Step 4. When this is done, save the output to the outputFile
+ */
+static void	process_exec(t_pipex *pipe, char **args)
+{
+	int		output;
+	int		input;
+	int		err;
+
+	input = dup2(pipe->fd_input, STDIN_FILENO);
+	output = dup2(pipe->fd_output, STDOUT_FILENO);
+	err = execve("/bin/ls", args, pipe->env);
+	if (err == -1)
+		perror(args[0]);
+}
+
 /*
  * The process_start() function executes the command determined by
  * char *cmd, then catches and returns the stdout of that command.
