d7bf55de705c7cdd781667e9c81bd409862cae59
Navi Ismail <mailvoornavi@gmail.com>
2022-02-06 17:44:38 +0100

    added parser to grab the PATH= variable from `**env`

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,11 +6,24 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/06 16:47:48 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/06 17:43:37 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
+
+/*
+ * The pipe_parse_env() function ...
+ */
+char	*pipe_parse_env(t_pipex *pipex)
+{
+	while (ft_strncmp("PATH", *pipex->env, 4))
+	{
+		pipex->env++;
+	}
+	pipex->path = (*pipex->env + 5);
+	return (pipex->path);
+}
 
 /*
  * The pipex program ...
@@ -25,6 +38,7 @@ int	main(int argc, char **args, char **env)
 	res = pipe_initialize(&pipe, args, (argc - 1), env);
 	if (!res)
 		return (0);
+	pipe_parse_env(&pipe);
 	pipe_run(&pipe);
 	pipe_close(&pipe);
 	return (0);
