b7b996f7ce20423579981504df8786b73fc7a43c
Navi Ismail <mailvoornavi@gmail.com>
2022-02-07 12:49:26 +0100

    fixed`process_exec` func to take arg of cmd id

--- srcs/modules/process.c
+++ srcs/modules/process.c
@@ -14,43 +14,29 @@
 
 /**
  * The process_exec() function ...
- * 
- * Step 1. Execute command in execve
- * Step 2. Save the output to the struct
- * Step 3. Use the output on the next command
- * ...
- * Step 4. When this is done, save the output to the outputFile
  */
-static void	process_exec(t_pipex *pipex, char **args)
+static void	process_exec(t_pipex *pipex, int id)
 {
 	int		err;
 	char	*cmd;
+	char	**args;
 
-	dup2(pipex->pipes[1], STDOUT_FILENO);
+	args = ft_split(pipex->cmds[id], ' ');
 	cmd = env_cmd(pipex, args[0]);
+	if (cmd == 0)
+		cmd = pipex->cmds[0];
 	err = execve(cmd, args, pipex->env);
-	if (err == -1)
-		perror(args[0]);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(": command doesn't exist.\n", STDERR_FILENO);
+	exit(127);
 }
 
-/*
- * The process_start() function executes the command determined by
- * char *cmd, then catches and returns the stdout of that command.
  */
-int	process_start(t_pipex *pipex, int id)
 {
 	int		pid;
-	char	**args;
 
 	pipe(pipex->pipes);
 	pid = fork();
-	if (pid == 0)
 	{
-		args = ft_split(pipex->cmds[id], ' ');
-		process_exec(pipex, args);
-		exit(pid);
 	}
-	while (wait(NULL) != -1 || errno != ECHILD)
-		;
-	return (1);
 }
