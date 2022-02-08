5089467809990561a339d2cd9d0147907cfbe416
Navi Ismail <mailvoornavi@gmail.com>
2022-02-08 16:58:18 +0100

    updated libft

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/13 08:33:59 by nismail       #+#    #+#                 */
-/*   Updated: 2021/12/05 15:56:16 by navi          ########   odam.nl         */
+/*   Updated: 2022/02/08 16:47:47 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ size_t	ft_strlcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t n);
 int		ft_strcmp(const char *str1, const char *str2);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_chrncmp(const char *str, const char c, int n);
+int		ft_chrcmp(const char *str, const char c);
 char	*ft_strrev(char *str);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
@@ -51,6 +53,7 @@ char	*ft_strtolower(char *str);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_realloc(void *ptr, size_t size);
+char	**ft_realloc_dp(char **str, int len);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *str, unsigned int index, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
